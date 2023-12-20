ef934dc075904247e54aac6d813ce65fef936bb7
containerscrew <131241415+containerscrew@users.noreply.github.com>
2023-12-20 12:25:50 +0100

    Wip

--- scripts/vscode-extensions.sh
+++ scripts/vscode-extensions.sh
@@ -2,9 +2,10 @@
 
 extensions=(rust-lang.rust-analyzer mvllow.rose-pine naumovs.color-highlight \
             PKief.material-icon-theme golang.Go christian-kohler.path-intellisense \
-            ionutvmi.path-autocomplete HashiCorp.terraform ms-python.python bungcip.better-toml AykutSarac.jsoncrack-vscode)
+            ionutvmi.path-autocomplete HashiCorp.terraform ms-python.python bungcip.better-toml AykutSarac.jsoncrack-vscode \
+            Equinusocio.vsc-community-material-theme)
 
 for extension in "${extensions[@]}"
 do
-  code --install-extension "$extension"
+  code --install-extension --force "$extension"
 done
