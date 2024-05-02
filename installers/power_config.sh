5392912393891354ca9b2cb32b35a586f0ec2447
containerscrew <131241415+containerscrew@users.noreply.github.com>
2024-05-02 10:11:37 +0200

    Enabel acpid systemd service

--- installers/power_config.sh
+++ installers/power_config.sh
@@ -5,6 +5,7 @@ source installers/logger.sh
 
 ############### ACPID ###############
 log_message "info" "Setup acpid events"
+sudo systemctl enable acpid --now
 sudo cp etc/acpi/events/* /etc/acpi/events/
 sudo chmod 755 etc/acpi/*.sh
 sudo cp etc/acpi/*.sh /etc/acpi/
