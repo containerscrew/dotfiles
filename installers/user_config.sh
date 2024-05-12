de5f05c2290ad25e584d2d46d4a093f69c3b753f
containerscrew <131241415+containerscrew@users.noreply.github.com>
2024-05-12 10:22:12 +0200

    wip

--- installers/user_config.sh
+++ installers/user_config.sh
@@ -78,3 +78,15 @@ fi
 
 # TFENV permissions
 sudo usermod -aG tfenv dcr
+
+# Vscode extensions
+log_message "info" "Setup vscode extensions"
+extensions=(rust-lang.rust-analyzer mvllow.rose-pine naumovs.color-highlight \
+            PKief.material-icon-theme golang.Go christian-kohler.path-intellisense \
+            ionutvmi.path-autocomplete HashiCorp.terraform ms-python.python bungcip.better-toml AykutSarac.jsoncrack-vscode \
+            Equinusocio.vsc-community-material-theme)
+
+for extension in "${extensions[@]}"
+do
+  code --install-extension "$extension" --force
+done
